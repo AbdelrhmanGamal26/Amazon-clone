dda9ef077ec3779f1fcbcd8f043aea602df09f31
Abdelrahman Gamal <abdelrhman.gamal53@gmail.com>
2023-11-15 03:39:50 +0200

    minor changes

---
+++ src/firebase/firebase.js
@@ -0,0 +1,26 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getDatabase } from "firebase/database";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: "clone-db7ee.firebaseapp.com",
+  databaseURL: "https://clone-db7ee-default-rtdb.firebaseio.com",
+  projectId: "clone-db7ee",
+  storageBucket: "clone-db7ee.appspot.com",
+  messagingSenderId: "654967322131",
+  appId: "1:654967322131:web:a809e0b9b578e36f4b71e6",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Initialize Firebase Authentication and get a reference to the service
+export const auth = getAuth(app);
+export const db = getDatabase(app);
+export default app;
